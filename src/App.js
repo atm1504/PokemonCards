27e4de5dfe4d9efe291685b8a0b11acae9677ec2
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-01 23:54:03 +0530

    feat: Fancy cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-        <h1><Pokecard id={4} name="Charmander" type="fire" exp={62}/></h1>
+        <Pokedex/>
       </div>
     );
   }
